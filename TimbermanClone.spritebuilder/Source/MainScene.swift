7977f4006ec7a59a96824ffa463d92f91f0ef899
Maria Luisa <marialuisa@Marias-MacBook-Pro.local>
2015-02-24 05:33:53 -0800

    Add random branches based on 50% chance

--- TimbermanClone.spritebuilder/Source/MainScene.swift
+++ TimbermanClone.spritebuilder/Source/MainScene.swift
@@ -8,23 +8,34 @@ class MainScene: CCNode {
     
     let pointsThatCharacterOverlapsWithBase: CGFloat = 2
     let treeBaseWidth: CGFloat = 128
+    let treeBaseHeight: CGFloat = 26
+    let treeSectionHeight: CGFloat = 80
     var mainSceneWidth: CGFloat!
+    var mainSceneHeight: CGFloat!
+    var treeSections = [TreeSection]()
     
     var physicsNode: CCPhysicsNode!
     var character: CCSprite!
+    
+    var btnRestart: CCButton!
     
 
     func didLoadFromCCB() {
         // Assign scene width to position character with respect to the center
         mainSceneWidth = CCDirector.sharedDirector().viewSize().width
+        mainSceneHeight = CCDirector.sharedDirector().viewSize().height
         
         // Load the character
         character = CCBReader.load("Character") as CCSprite
-        self.addChild(character)
+        physicsNode.addChild(character)
         moveCharacterTo(.Left)
         
-        // Add tree sections to form the tree
-        addTreeSection()
+        // Add tree sections until the tree fills the screen
+        while ((CGFloat(treeSections.count) * treeSectionHeight + treeBaseHeight) < mainSceneHeight) {
+            addTreeSection()
+        }
+        
+        
         
         // Allow user input
         self.userInteractionEnabled = true
@@ -57,9 +68,19 @@ class MainScene: CCNode {
     }
     
     func addTreeSection() {
-        let treeSection = TreeSection()
+        let treeSection = CCBReader.load("TreeSection") as TreeSection
         
+        treeSection.positionType.xUnit = CCPositionUnit.Normalized
+        treeSection.position.x = 0.5
+        treeSection.position.y = treeBaseHeight + treeSectionHeight * CGFloat(treeSections.count)
+
+        // Add branches only if the first 2 sections without branches have been added
+        if treeSections.count >= 2 {
+            treeSection.addBranch()
+        }
         
+        physicsNode.addChild(treeSection)
+        treeSections.append(treeSection)
     }
     
 
--- TimbermanClone.spritebuilder/Source/TreeSection.swift
+++ TimbermanClone.spritebuilder/Source/TreeSection.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class TreeSection: CCNode {
 
-
+    var leftBranchContainer: CCNode!
+    var rightBranchContainer: CCNode!
     
+    func addBranch() {
+        let probability = arc4random() % 100
+        NSLog("probability: \(probability)")
+        if probability < 50 {
+            addBranchAt(.Left)
+        } else {
+            addBranchAt(.Right)
+        }
+    }
+    
+    func addBranchAt(side: Position) {
+        let branch = CCSprite.spriteWithImageNamed("assets/branch.png") as CCSprite
+        branch.position.x = 2
+        
+        if side == .Left {
+            branch.flipX = true
+            branch.positionType = CCPositionTypeMake(CCPositionUnit.Points, CCPositionUnit.Points, CCPositionReferenceCorner.BottomRight)
+            branch.anchorPoint = ccp(1, 0)
+            leftBranchContainer.addChild(branch)
+        } else if side == .Right {
+            branch.anchorPoint = ccp(0, 0)
+            rightBranchContainer.addChild(branch)
+        }
+    }
 }
